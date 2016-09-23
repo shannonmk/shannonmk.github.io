b2cc1aa4bf2fc42f70fefa3ccf0c73cc55278987
shannonmk <skellius77@gmail.com>
2016-09-22 19:38:24 -0500

    Update navbar.js
    Fixed naming error

--- javascripts/navbar.js
+++ javascripts/navbar.js
@@ -1,5 +1,5 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function navbarFunction() {
+function topnav Function() {
 var x = document.getElementById("myTopnav");
 if (x.className === "topnav") {
   x.className += " responsive";
